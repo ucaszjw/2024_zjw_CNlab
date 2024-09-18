be4100c2c9f3f9563e46e016db5515734d680dc1
ucaszjw <zhangjiawei2004@126.com>
2024-09-18 17:31:35 +0800

    finish lab03

--- Lab03/03-socket/03-socket/http-server.c
+++ Lab03/03-socket/03-socket/http-server.c
@@ -133,8 +133,9 @@ void* handle_http_request(void* arg){
     int request = *(int*)arg;
 
     char* recv_buff = (char*)malloc(2000 * sizeof(char));
-    char* send_buff = (char*)malloc(2000 * sizeof(char));
+    char* send_buff = (char*)malloc(6000 * sizeof(char));
     memset(recv_buff, 0, 2000);
+    memset(send_buff, 0, 6000);
 
     int request_len = recv(request, recv_buff, 2000, 0);
     if (request_len < 0){
@@ -150,6 +151,211 @@ void* handle_http_request(void* arg){
         char *host = (char*)malloc(100 * sizeof(char));
         int relative_url = *pos == '/';
 
-        for (int i = 0; *pos != )
+        for (int i = 0; *pos != ' '; pos++, i++)
+            url[i] = *pos;
+        url[i] = "\0";
+        pos++;
+
+        for (int i = 0; *pos != '\r'; pos++, i++)
+            http_version[i] = *pos;
+        http_version[i] = '\0';
+
+        if (relative_url){
+            pos = strstr(recv_buff, "Host:");
+            if (!pos){
+                printf("HTTP host not found");
+                return -1;
+            }
+            pos += 6;
+
+            for (int i = 0; *pos != '\r'; pos++, i++)
+                host[i] = *pos;
+            host[i] = '\0';
+        }
+
+
+        strcat(send_buff, host);
+        strcat(send_buff, http_version);
+        strcat(send_buff, " 301 Moved Permanently\r\nLocation");
+        strcat(send_buff, "https://");
+
+        if (relative_url){
+            strcat(send_buff, host);
+            strcat(send_buff, url);
+        }
+        else
+            strcat(send_buff, &url[7]);
+        strcat(send_buff, "\r\n\r\n\r\n\r\n");
+
+        if (send(request, send_buff, strlen(send_buff), 0) < 0){
+            printf("HTTP send failed");
+            return -1;
+        }
+
+        free(url);
+        free(http_version);
+        free(host);
     }
+    free(send_buff);
+    free(recv_buff);
+
+    close(request);
+    return NULL;
+}
+
+void* handle_https_request(void* arg){
+    pthread_detach(pthread_self());
+    SSL *ssl = (SSL*)arg;
+    if (SSL_accept(ssl == -1)){
+        printf("HTTPS SSL_accept fialed");
+        return -1;
+    }
+
+    char* recv_buff = (char*)malloc(2000 * sizeof(char));
+    char* send_buff = (char*)malloc(6000 * sizeof(char));
+    int keep_alive = 1;
+
+    while (keep_alive){
+        memset(recv_buff, 0, 2000);
+        int request_len = SSL_read(ssl, recv_buff, 2000);
+        if (request_len < 0){
+            printf("HTTPS SSL_read failed");
+            return -1;
+        }
+        if (recv_buff[0] == '\0')
+            break;
+
+        char *url = (char*)malloc(50 * sizeof(char));
+        char *http_version = (char*)malloc(9 * sizeof(char));
+        char *path = (char*)malloc(100 * sizeof(char));
+        char *get = strstr(recv_buff, "GET");
+
+        if (get){
+            char *pos = get + 4;
+            int relative_url = *iterator == '/'; 
+            int range = 0;
+            int range_begin, range_end;
+
+            for (int i = 0; *pos != ' '; pos++, i++)
+                url[i] = *pos;
+            url[i] = '\0';
+            pos++;
+
+            for (int i = 0; *pos != '\r'; pos++, i++)
+                http_version[i] = *pos;
+            http_version[i] = '\0';
+
+            if (pos = strstr(recv_buff, "Range:")){
+                pos += 13;
+                range = 1;
+                range_begin = 0;
+
+                while(*pos >= '0' && *pos <= '9'){
+                    range_begin = range_begin * 10 + *pos - '0';
+                    pos++;
+                }
+                pos++;
+
+                if (*pos < '0' || *pos > '9')
+                    range_end = -1;
+                else{
+                    range_end = 0;
+                    while(*pos >= '0' && *pos <= '9'){
+                        range_end = range_end * 10 + *pos - '0';
+                        pos++;
+                    }
+                }
+            }
+
+            if (pos = strstr(recv_buff, "Connection:")){
+                pos += 12;
+                if (*pos = "k")
+                    keep_alive = 1;
+                else
+                    keep_alive = 0;
+            }
+
+            path[0] = '.';
+            path[1] = '\0';
+            if (relative_url)
+                strcat(path, url);
+            else{
+                int count = 3;
+                for (int i = 0; count > 0; i++){
+                    if (url[i] == '/')
+                        count--;
+                }
+                strcat(path, &url[i]);
+            }
+
+
+            FILE *fp = fopen(path, "r");
+            if (!fp){
+                memset(send_buff, 0, 6000);
+                strcat(send_buff, http_version);
+                strcat(send_buff, " 404 Not Found\r\n\r\n\r\n\r\n");
+                SSL_write(ssl, send_buff, strlen(send_buff));
+                break;
+            }
+            else{
+                char header[200] = {0};
+                strcat(header, http_version);
+                if (range)
+                    strcat(header, " 206 Partial Content\r\n");
+                else
+                    strcat(header, " 200 OK\r\n");
+
+                int file_size, file_begin;
+                if (range){
+                    if(range_end == -1){
+                        fseek(fp, 0L, SEEK_END);
+                        file_size = ftell(fp) - range_begin + 1;
+                        file_begin = range_begin;
+                    }
+                    else{
+                        file_size = range_end - range_begin + 1;
+                        file_begin = range_begin;
+                    }
+                }
+                else{
+                    fseek(fp, 0L, SEEK_END);
+                    file_size = ftell(fp);
+                    file_begin = 0;
+                }
+
+                strcat(header, "Content-Length: ");
+                fseek(fp, file_begin, 0);
+                char file_size_str[64] = {0};
+                sprintf(file_size_str, "%d", file_size);
+
+                char *response = (char*)malloc((file_size + 200) * sizeof(char));
+                memset(response, 0, file_size + 200);
+                strcat(response, header);
+                strcat(response, file_size_str);
+
+                strcat(response, "\r\nConnection: ");
+                if (keep_alive)
+                    strcat(response, "keep-alive");
+                else
+                    strcat(response, "close");
+                
+                strcat(response, "\r\n\r\n");
+                fread(response + strlen(response), 1, file_size, fp);
+                SSL_write(ssl, response, strlen(response));
+
+                fclose(fp);
+                if(range == 1 && range_end == -1)
+                    break;
+            }
+        }
+        free(url);
+        free(http_version);
+        free(path);
+    }
+    free(recv_buff);
+    free(send_buff);
+
+    SSL_free(ssl);
+    close(SSL_get_fd(ssl));
+    return NULL;
 }
