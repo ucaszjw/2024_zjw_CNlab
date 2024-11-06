c11b15f6ebac27f58331bc5c845ccf6b3c5cf76a
ucaszjw <zhangjiawei2004@126.com>
2024-11-06 21:56:31 +0800

    revise ip.c

--- Lab09/09-mospf/ip.c
+++ Lab09/09-mospf/ip.c
@@ -41,6 +41,27 @@ void handle_ip_packet(iface_info_t *iface, char *packet, int len)
 		free(packet);
 	}
 	else {
-		ip_forward_packet(daddr, packet, len);
+		ip->ttl--;
+		if (ip->ttl <= 0) {
+			icmp_send_packet(packet, len, ICMP_TIME_EXCEEDED, ICMP_EXC_TTL);
+			free(packet);
+			return;
+		}
+
+		ip->checksum = ip_checksum(ip);
+		rt_entry_t *match = longest_prefix_match(daddr);
+		if (match == NULL) {
+			icmp_send_packet(packet, len, ICMP_DEST_UNREACH, ICMP_NET_UNREACH);
+			free(packet);
+			return;
+		}
+
+		u32 nextip;
+		if (match->gw == 0)
+			nextip = daddr;
+		else
+			nextip = match->gw;
+
+		iface_send_packet_by_arp(match->iface, nextip, packet, len);
 	}
 }
